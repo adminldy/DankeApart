64b29d60298d2981bae47803f9731ee00732f816
Ldy <61314547+adminldy@users.noreply.github.com>
2021-09-06 23:38:04 +0800

    TabBar样式

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,50 +1,55 @@
 import React from "react"
 import { Route } from 'react-router-dom'
 import News from "../News"
+import Index from "../Index";
+import Profile from "../Profile";
+import HouseList from "../HouseList";
 import { TabBar } from 'antd-mobile';
 import './index.css'
 export default class Home extends React.Component {
   state = {
     //默认选中的tabbar菜单项
-    selectedTab: 'redTab',
+    selectedTab: this.props.location.pathname,
     //用于控制TabBar的展示与隐藏, 这个值应该是false， 也就是不隐藏。
     // hidden: false,
     // 全屏
     // fullScreen: false,
   }
   // 渲染每个TabBar内容
-  renderContent(pageText) {
-    return (
-      <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
-        <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
-        <a style={{ display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9' }}
-          onClick={(e) => {
-            e.preventDefault();
-            this.setState({
-              hidden: !this.state.hidden,
-            });
-          }}
-        >
-          Click to show/hide tab-bar
-        </a>
-        <a style={{ display: 'block', marginBottom: 600, color: '#108ee9' }}
-          onClick={(e) => {
-            e.preventDefault();
-            this.setState({
-              fullScreen: !this.state.fullScreen,
-            });
-          }}
-        >
-          Click to switch fullscreen
-        </a>
-      </div>
-    );
-  }
+  // renderContent(pageText) {
+  //   return (
+  //     <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
+  //       <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
+  //       <a style={{ display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9' }}
+  //         onClick={(e) => {
+  //           e.preventDefault();
+  //           this.setState({
+  //             hidden: !this.state.hidden,
+  //           });
+  //         }}
+  //       >
+  //         Click to show/hide tab-bar
+  //       </a>
+  //       <a style={{ display: 'block', marginBottom: 600, color: '#108ee9' }}
+  //         onClick={(e) => {
+  //           e.preventDefault();
+  //           this.setState({
+  //             fullScreen: !this.state.fullScreen,
+  //           });
+  //         }}
+  //       >
+  //         Click to switch fullscreen
+  //       </a>
+  //     </div>
+  //   );
+  // }
   render() {
     return <div className="home">
       {/* 以父路由path开头 */}
       <Route path="/home/news" component={News}></Route>
-      
+      <Route path="/home/index" component={Index}></Route>
+      <Route path="/home/profile" component={Profile}></Route>
+      <Route path="/home/list" component={HouseList}></Route>
         <TabBar
           //未选中颜色
           unselectedTintColor="#888888"
@@ -52,6 +57,7 @@ export default class Home extends React.Component {
           tintColor="#3172F4"
           barTintColor="white"
           // hidden={this.state.hidden}
+          noRenderContent={true}
         >
           <TabBar.Item
             title="首页"
@@ -62,15 +68,15 @@ export default class Home extends React.Component {
             selectedIcon={
               <i className="iconfont icon-ind" />
             }
-            selected={this.state.selectedTab === 'blueTab'}
+            selected={this.state.selectedTab === '/home/index'}
             onPress={() => {
               this.setState({
-                selectedTab: 'blueTab',
+                selectedTab: '/home/index',
               });
+              this.props.history.push('/home/index')
             }}
             data-seed="logId"
           >
-            {this.renderContent('Life')}
           </TabBar.Item>
           <TabBar.Item
             icon={
@@ -81,15 +87,15 @@ export default class Home extends React.Component {
             }
             title="找房"
             key="Koubei"
-            selected={this.state.selectedTab === 'redTab'}
+            selected={this.state.selectedTab === '/home/list'}
             onPress={() => {
               this.setState({
-                selectedTab: 'redTab',
+                selectedTab: '/home/list',
               });
+              this.props.history.push('/home/list')
             }}
             data-seed="logId1"
           >
-            {this.renderContent('Koubei')}
           </TabBar.Item>
           <TabBar.Item
             icon={
@@ -99,15 +105,15 @@ export default class Home extends React.Component {
               <i className="iconfont icon-infom" />
             }
             title="资讯"
-            key="Friend"
-            selected={this.state.selectedTab === 'greenTab'}
+            key="Friend" 
+            selected={this.state.selectedTab === '/home/news'}
             onPress={() => {
               this.setState({
-                selectedTab: 'greenTab',
+                selectedTab: '/home/news',
               });
+              this.props.history.push('/home/news')
             }}
           >
-            {this.renderContent('Friend')}
           </TabBar.Item>
           <TabBar.Item
             icon={
@@ -118,14 +124,14 @@ export default class Home extends React.Component {
             }
             title="我的"
             key="my"
-            selected={this.state.selectedTab === 'yellowTab'}
+            selected={this.state.selectedTab === '/home/profile'}
             onPress={() => {
               this.setState({
-                selectedTab: 'yellowTab',
+                selectedTab: '/home/profile',
               });
+              this.props.history.push('/home/profile')
             }}
           >
-            {this.renderContent('My')}
           </TabBar.Item>
         </TabBar>
     </div>
---
+++ src/pages/HouseList/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export default class HouseList extends React.Component {
+  render() {
+    return <div>列表找房</div>
+  }
+}
